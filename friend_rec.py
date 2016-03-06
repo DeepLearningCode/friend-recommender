1811cf0f4ac744424931ad1a3bc760732b84d4e2
unpairestgood <paulesingman@gmail.com>
2016-03-06 13:05:12 -0500

    Add recommender code

---
+++ friend_rec.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import sys
+
+
+class VenmoFriendGraph(object):
+    """Initialize a VenmoFriendGraph object to make friend recommendations"""
+    
+    def __init__(self,):
+        self.filename = 'Venmo_Homework_data.csv'
+        
+        # Put csv data into pandas DataFrame
+        self.df = self.load_data(self.filename)
+        
+        # Create networkx graph from DataFrame
+        self.G = self.make_network(self.df)
+         
+        
+    def load_data(self, filename):
+        """Put Venmo data into df if file exists"""
+         
+        try:
+            df = pd.read_csv(filename)
+        except IOError:
+            raise IOError('Check that file name and path are correct')
+            
+        return df 
+            
+    
+    def make_network(self, df):
+        """Take a DataFrame of undirected friendships and return networkx graph object"""
+        
+        G = nx.from_pandas_dataframe(df, df.columns[0], df.columns[1])
+        return G
+        
+    def friends_set(self, G, node):
+        """Returns a set of a given user's friends"""
+        return set(G.neighbors(node))
+    
+    def friends_of_friends_set(self, G, friends):
+        """Given a set of friends, returns a set of friends of friends"""
+        friends_of_friends = set()
+        
+        for f in friends:
+            friends_of_friends.update(G.neighbors(f))
+            
+        return friends_of_friends
+        
+    def make_remove_set(self, G, node, friends_of_friends):
+        """Removes friends of friends that a user is already friends with"""
+        remove_set = set()
+        
+        for fof in friends_of_friends:
+            if G.has_edge(node, fof):
+                remove_set.add(fof)
+                
+        return remove_set
+    
+   
+    def find_friends_of_friends(self, G, node):
+        """Given a user_id, find friends of friends the user is not friends with"""
+    
+        # find current friends of user
+        friends = self.friends_set(G, node)
+    
+        # find friends of friends
+        friends_of_friends = self.friends_of_friends_set(G, friends)
+        
+        # remove friend_of_friend if fof is friends with user
+        remove_set = self.make_remove_set(G, node, friends_of_friends)
+            
+        # Remove friends of friends that are in remove set
+        friends_of_friends.difference_update(remove_set)
+    
+        return list(friends_of_friends) 
+    
+    
+    def highest_degree(self, G, fof):
+        """Given list of friends of friends, return 5 with highest degree"""
+    
+        degree_dict = {}
+        # Calculate degree for each node, store result in dict
+        for f in fof:
+            deg = G.degree(f)
+            degree_dict[f] = deg
+        
+        # Sort by dict values
+        sorted_list = sorted(degree_dict, key=degree_dict.get, reverse=True)
+    
+        return sorted_list[:5]
+        
+    def format_friend_recs(self, user_id, recs):
+        """Print 5 friend recommendations to terminal"""
+        
+        print 'Friend recommendations for user {0}: {1}'.format(user_id,
+                    ', '.join(['#'+str(i+1)+': '+str(r) for i, r in enumerate(recs)]))
+     
+    
+    def recommend_friend(self, user_id):
+        """Given a user_id, make 5 friend recommendations"""
+        
+        # Find friends of friends of user
+        friends_of_friends = self.find_friends_of_friends(self.G, user_id)
+        
+        # Find 5 most connected friends of friends
+        recommendations = self.highest_degree(self.G, friends_of_friends)
+        
+        return self.format_friend_recs(user_id, recommendations)
+         
+
+def run():
+    """Load data, create networkx graph, and print friend recommendations"""
+    
+    if len(sys.argv) is 2: # user_id is input as command line argument
+        user_id = int(sys.argv[1]) # e.g. python friend_rec.py 59003
+    else: # otherwise must manually update user_id
+        user_id = 59003
+    
+    # Instantiate instance of VenmoFriendGraph object
+    v = VenmoFriendGraph()
+    
+    # make friend recs for specified user_id    
+    recs = v.recommend_friend(user_id)
+
+
+if __name__== '__main__':
+    run()
